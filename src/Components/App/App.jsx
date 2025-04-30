d5b04cc5aca294e795a1d67ae3e9e2c103d68c24
andf.750@gmail.com <andf.750@gmail.com>
2025-04-30 17:04:36 +0300

    saving

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -77,6 +77,8 @@ const routes = createBrowserRouter([
   }
 ]);
 
+
+
 function App() {
   return (
     <CartProvider>
--- src/Components/MasterLayout/MasterLayout.jsx
+++ src/Components/MasterLayout/MasterLayout.jsx
@@ -9,7 +9,7 @@ export default function MasterLayout() {
     <Navbar/>
     {/* <ClassComponent userName={this.state.userName}/> */}
     {/* <FunctionComponent userName={this.state.userName} userAge={this.state.userAge}/> */}
-    <div className='text-center container'>
+    <div className='text-center  container'>
       <Outlet>  </Outlet>
     </div>
     <Footer/>
